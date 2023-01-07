c4b630a593cf9e092c43f3ba5c916dddf9f8811b
NeeDKK <wzj.7.29@gmail.com>
2023-01-08 01:57:56 +0800

    Add java & golang docker build and script file for x86 & arm64

---
+++ influxdb-go/compile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+IMAGENAMEX86=influxdb-go-x86
+IMAGENAMEARM=influxdb-go-arm
+
+
+
+#jar包完成后编译x86版本
+docker build -f Dockerfile -t $IMAGENAMEX86:v1.0 .
+#编译完成后给镜像打tag
+IMAGEID=$(docker images |grep $IMAGENAMEX86 | awk 'NR==1{print}'|awk '{print $3}')
+docker tag $IMAGEID registry.cn-hangzhou.aliyuncs.com/needkk/$IMAGENAMEX86:v1.0
+#推送镜像到阿里云
+docker push registry.cn-hangzhou.aliyuncs.com/needkk/$IMAGENAMEX86:v1.0
+
+
+
+
+#编译arm版本
+docker build -f DockerfileArm -t $IMAGENAMEARM:v1.0 .
+#编译完成后给镜像打tag
+IMAGEIDARM=$(docker images |grep $IMAGENAMEARM | awk 'NR==1{print}'|awk '{print $3}')
+docker tag $IMAGEIDARM registry.cn-hangzhou.aliyuncs.com/needkk/$IMAGENAMEARM:v1.0
+#推送镜像到阿里云
+docker push registry.cn-hangzhou.aliyuncs.com/needkk/$IMAGENAMEARM:v1.0
